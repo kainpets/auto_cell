d69f96c6b0896d987a77a30275730643ba8481ae
Paweł Stępniak <pawelstepniak22@gmail.com>
2023-06-07 09:39:03 +0000

    add canvas

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+const app = document.getElementById("app");
+const ctx = app.getContext("2d");
+app.width = 800;
+app.height = 600;
+console.log(ctx);
---
+++ index.ts
@@ -0,0 +1,7 @@
+const app = document.getElementById("app") as HTMLCanvasElement
+const ctx = app.getContext("2d")
+
+app.width = 800
+app.height = 600
+
+console.log(ctx)
