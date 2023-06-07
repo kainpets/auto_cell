4d587061cbde510f33e296c9481974ee0d503be9
Paweł Stępniak <pawelstepniak22@gmail.com>
2023-06-07 09:55:36 +0000

    add css

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 "use strict";
+const BOARD_ROWS = 32;
+const BOARD_COLS = 32;
 const app = document.getElementById("app");
+if (!app)
+    throw new Error("No canvas found");
 const ctx = app.getContext("2d");
+if (!ctx)
+    throw new Error("Context couldn't be initialized");
 app.width = 800;
 app.height = 600;
+ctx.fillStyle = "#202020";
+ctx === null || ctx === void 0 ? void 0 : ctx.fillRect(0, 0, app.width, app.height);
 console.log(ctx);
--- index.ts
+++ index.ts
@@ -1,7 +1,15 @@
+const BOARD_ROWS = 32
+const BOARD_COLS = 32
+
 const app = document.getElementById("app") as HTMLCanvasElement
+if (!app) throw new Error("No canvas found")
 const ctx = app.getContext("2d")
+if (!ctx) throw new Error("Context couldn't be initialized")
 
 app.width = 800
 app.height = 600
+
+ctx.fillStyle = "#202020"
+ctx?.fillRect(0, 0, app.width, app.height)
 
 console.log(ctx)
